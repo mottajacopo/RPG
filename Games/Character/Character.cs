8d0578b3d89bc9efabb646a72979c3d19d092c34
michisalvo <jimmyginooo@libero.it>
2016-11-17 21:45:53 +0100

    Add class Character

---
+++ Games/Character/Character.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Characters
+{
+    public class Character
+    {
+        private string nickname;
+
+        public string Nickname
+        {
+            get
+            {
+                return nickname;
+            }
+            private set
+            {
+                nickname = value;
+            }
+        }
+
+        public Character(string nickname)
+        {
+            Nickname = nickname;
+        }
+    }
+}
