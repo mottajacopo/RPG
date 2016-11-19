e57d6716bf68efc1348a7d501f69680f8880410a
michisalvo <jimmyginooo@libero.it>
2016-11-19 18:31:46 +0100

    Add Item,Attribute,Location class

---
+++ Games/Engine/Item.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+
+    class Item
+    {
+        private string name;
+        public string ItemName
+        {
+            get
+            {
+                return name;
+            }
+            private set
+            {
+                name = value;
+            }
+        }
+
+        private string pluralName;
+        public string ItemPluralName
+        {
+            get
+            {
+                return pluralName;
+            }
+            private set
+            {
+                pluralName = value;
+            }
+        }
+
+
+        private int id;
+        public int ID
+        {
+            get
+            {
+                return id;
+            }
+            private set
+            {
+                id = value;
+            }
+        }
+
+        public Item(string name,string pluralName,int id)
+        {
+            ItemName = name;
+            ItemPluralName = pluralName;
+            ID = id;
+        }
+
+    }
+}
---
+++ Games/Engine/Location.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+    class Location
+    {
+        private string placeName;
+        public string PlaceName
+        {
+            get
+            {
+                return placeName;
+            }
+            private set
+            {
+                placeName = value;
+            }
+        }
+
+        public Location(string name)
+        {
+             PlaceName = name;
+
+        }
+    }
+}
